586ee5c93f266e5c794876b6f3690993639b424d
kawayiYokami <289104862@qq.com>
2026-02-01 11:37:41 +0800

    隐藏debug内容

--- web/optimizer/vite.config.ts
+++ web/optimizer/vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+  },
   server: {
     port: 8522,
   },
